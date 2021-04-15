03ea00c907e53569b497132e115c14dd70385c03
Nikita Burak <nikitos.burak@gmail.com>
2021-04-15 21:22:54 +0300

    add category

---
+++ src/main/java/guru/springfamework/springmvcrestv2/api/v2/CategoryDTO.java
@@ -0,0 +1,6 @@
+package guru.springfamework.springmvcrestv2.api.v2;
+
+public class CategoryDTO {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/guru/springfamework/springmvcrestv2/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springfamework.springmvcrestv2.repositories;
+
+import guru.springfamework.springmvcrestv2.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
