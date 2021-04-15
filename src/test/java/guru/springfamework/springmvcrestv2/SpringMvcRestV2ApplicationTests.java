f7aced98f747708a5caa60721757223bd464ce72
Nikita Burak <nikitos.burak@gmail.com>
2021-04-15 21:21:59 +0300

    initial commit

---
+++ src/test/java/guru/springfamework/springmvcrestv2/SpringMvcRestV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springfamework.springmvcrestv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
