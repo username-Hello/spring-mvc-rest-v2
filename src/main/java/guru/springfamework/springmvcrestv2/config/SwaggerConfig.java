43503c5245f9da6f489aee7660eb3c61f0b29577
username-Hello <nikitos.burak@gmail.com>
2021-04-29 11:50:51 +0300

    add Meta Data

--- src/main/java/guru/springfamework/springmvcrestv2/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/springmvcrestv2/config/SwaggerConfig.java
@@ -1,10 +1,47 @@
 package guru.springfamework.springmvcrestv2.config;
 
+import com.sun.deploy.ui.AppInfo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(getMetaData());
+    }
+
+    public ApiInfo getMetaData() {
+        Contact contact = new Contact("Nikita Burak", "https://springframework.guru/about/",
+                "nikitos.burak@gmail.com");
+
+        return new ApiInfo(
+                "SFG",
+                "Spring Framework 5: Beginner to Guru",
+                "1.0",
+                "Terms of service: blah blah",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList());
+    }
 
 }
