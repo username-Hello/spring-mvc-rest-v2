e7fbcef6a899665abe17f7a40577b32702479fc3
username-Hello <nikitos.burak@gmail.com>
2021-04-29 10:07:25 +0300

    add Swagger Config

---
+++ src/main/java/guru/springfamework/springmvcrestv2/config/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package guru.springfamework.springmvcrestv2.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+}
